b80179ec60442790a014541a618bd0a7a174cf05
evangile2vie <evangile2vie@gmail.com>
2017-10-23 21:22:23 +0200

    Adding new prototypes of methods

--- app/src/main/java/lveapp/fr/materialdesign/View/Interfaces/NavDrawerView.java
+++ app/src/main/java/lveapp/fr/materialdesign/View/Interfaces/NavDrawerView.java
@@ -5,4 +5,20 @@ package lveapp.fr.materialdesign.View.Interfaces;
  */
 
 public class NavDrawerView {
+    public interface INavDrawer{
+        /**
+         * Initialize widgets
+         */
+        public void initialize();
+
+        /**
+         * Manage Widgets events
+         */
+        public void events();
+    }
+
+    /**
+     * Presenter
+     */
+    public interface IPresenter{}
 }
